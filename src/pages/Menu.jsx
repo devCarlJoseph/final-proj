3f4e86efba293bca24f7ee71399ac2a7ea5de62c
unknown <dj.degamo.p@gmail.com>
2025-05-02 17:15:31 +0800

    new changes

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,6 +2,7 @@ import Header from "../shared/components/Partials/Header";
 import Hero from "../components/Menu/MenuHero";
 import Sweets from "../components/Menu/TopList";
 import SweetsItems from "../components/Menu/SweetsItems";
+import MenuSlide from "../components/Menu/MenuSlide";
 
 export default function Menu() {
   return (
@@ -9,6 +10,7 @@ export default function Menu() {
       <Header />
       <Hero />
       <Sweets />
+      <MenuSlide />
       <SweetsItems />
     </div>
   );
